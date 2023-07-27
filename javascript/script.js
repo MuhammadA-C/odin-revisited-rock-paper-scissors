2cb140294da8a3c62ea248f59582a3e6fc89f0b1
MuhammadA-C <phantomganhdi@gmail.com>
2023-07-26 20:30:06 -0700

    Basic Game Working
    Created the code to play the game and update the results in the console.
    
    Then once the max number of rounds is reached the game will end and an alert box pop up.

--- javascript/script.js
+++ javascript/script.js
@@ -11,28 +11,45 @@ const scissorsCard = document.querySelector(".cards > :last-child");
 const announceWinner = document.querySelector(".annoucement > :first-child");
 const announceRound = document.querySelector(".annoucement > :last-child");
 const delayInMiliSeconds = 100;
-let selectedCard = "";
+const maxRounds = 5;
+let roundNumber = 0;
+let playerSelectedCard = 0;
 
 rockCard.addEventListener("click", () => {
+  playerSelectedCard = 1;
+
   turnOffRockCardHoverEffect();
+  game(playerSelectedCard);
   setTimeout(turnOffRockCardHoverEffect, delayInMiliSeconds);
-  selectedCard = "rock";
-  console.log("Rock");
 });
 
 paperCard.addEventListener("click", () => {
+  playerSelectedCard = 2;
+
   turnOffPaperCardHoverEffect();
+  game(playerSelectedCard);
   setTimeout(turnOffPaperCardHoverEffect, delayInMiliSeconds);
-  selectedCard = "paper";
-  console.log("Paper");
 });
 
 scissorsCard.addEventListener("click", () => {
+  playerSelectedCard = 3;
+
   turnOffScissorsCardHoverEffect();
+  game(playerSelectedCard);
   setTimeout(turnOffScissorsCardHoverEffect, delayInMiliSeconds);
-  selectedCard = "scissors";
-  console.log("Scissors");
 });
+
+function getRock() {
+  return 1;
+}
+
+function getPaper() {
+  return 2;
+}
+
+function getScissors() {
+  return 3;
+}
 
 function turnOffRockCardHoverEffect() {
   rockCard.classList.toggle("card-hover-effect");
@@ -46,4 +63,47 @@ function turnOffScissorsCardHoverEffect() {
   scissorsCard.classList.toggle("card-hover-effect");
 }
 
-function game(selectedCard) {}
+function randomNumberGenerator() {
+  const maxNumber = 3;
+  return Math.floor(Math.random() * maxNumber) + 1;
+}
+
+function didYouWin(number1, number2) {
+  if (number1 === getRock() && number2 === getScissors()) {
+    return true;
+  } else if (number1 === getPaper() && number2 === getRock()) {
+    return true;
+  } else if (number1 === getScissors() && number2 === getPaper()) {
+    return true;
+  }
+  return false;
+}
+
+function didYouTie(number1, number2) {
+  if (number1 === number2) {
+    return true;
+  }
+  return false;
+}
+
+function game(playerSelectedCard) {
+  let computerSelectedCard = randomNumberGenerator();
+
+  if (roundNumber >= maxRounds) {
+    alert("Game Over");
+    roundNumber = 0;
+    return;
+  }
+
+  if (didYouWin(playerSelectedCard, computerSelectedCard)) {
+    console.log("You Win!");
+  } else if (didYouTie(playerSelectedCard, computerSelectedCard)) {
+    console.log("You Tied");
+  } else if (didYouWin(computerSelectedCard, playerSelectedCard)) {
+    console.log("You Lose!");
+  } else {
+    console.log("Error");
+  }
+
+  roundNumber++;
+}
